41f7b030b2a76e74d4433f3673707f58b4b98508
Newton <altontonnalumasa@gmail.com>
2022-10-25 21:29:41 +0300

    add sql for previous point

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (ID, NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG) VALUES(1, 'Agumon', '2020-02-03', 0, true, 10.23);
+INSERT INTO animals (ID, NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG) VALUES(2, 'Gabumon', '2018-11-15', 2, true, 8.00);
+INSERT INTO animals (ID, NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG) VALUES(3, 'Pikachu', '2021-01-07', 1, false, 15.04);
+INSERT INTO animals (ID, NAME, DATE_OF_BIRTH, ESCAPE_ATTEMPTS, NEUTERED, WEIGHT_KG) VALUES(4, 'Devimon', '2017-05-12', 5, true, 11.00);
--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    ID INT PRIMARY KEY NOT NULL,
+    NAME TEXT NOT NULL,
+    DATE_OF_BIRTH DATE,
+    ESCAPE_ATTEMPTS INT,
+    NEUTERED BOOLEAN,
+    WEIGHT_KG DECIMAl
 );
