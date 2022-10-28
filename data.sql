0c0819fbc5a8db79592fcd444d1729b84462671b
Newton <altontonnalumasa@gmail.com>
2022-10-28 13:37:26 +0300

    work on multiple tables using join

--- data.sql
+++ data.sql
@@ -10,3 +10,14 @@ INSERT INTO ANIMALS VALUES (8, 'Angemon', '2005-06-12', 1, true, -45.00);
 INSERT INTO ANIMALS VALUES (9, 'Boarmon', '2005-07-07', 7, true, 20.4);
 INSERT INTO ANIMALS VALUES (10, 'Blossom', '1998-10-13', 3, true, 17.00);
 INSERT INTO ANIMALS VALUES (11, 'Dito', '2022-05-14', 4, true, 22.00);
+
+-- Multiple Tables --
+INSERT INTO OWNERS (full_name, age) VALUES ('Sam Smith', 34), ('Jennifer Orwel', 19), ('Bob', 45), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+UPDATE ANIMALS SET SPECIES_ID = 2 WHERE NAME LIKE '%mon%';
+UPDATE ANIMALS SET SPECIES_ID = 1 WHERE ID IN (3, 10, 11, 5, 7);
+UPDATE ANIMALS SET OWNER_ID = 1 WHERE NAME = 'Agumon';
+UPDATE ANIMALS SET OWNER_ID = 2 WHERE NAME IN ('Gabumon', 'Pikachu');
+UPDATE ANIMALS SET OWNER_ID = 3 WHERE NAME IN ('Devimon', 'Plantmon');
+UPDATE ANIMALS SET OWNER_ID = 4 WHERE NAME IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE ANIMALS SET OWNER_ID = 5 WHERE NAME IN ('Angemon', 'Boarmon');
